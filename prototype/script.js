f0addd3d3f68fb40e2fa59bdea6b1e83fa40b697
APAXIC <andrii.vus@gmail.com>
2026-04-01 22:40:37 +0100

    added business page from Eimantas and integrated my about page

--- prototype/script.js
+++ prototype/script.js
@@ -25,3 +25,39 @@ document.addEventListener('DOMContentLoaded', function () {
     year.textContent = new Date().getFullYear();
   }
 });
+
+const solutionItems = document.querySelectorAll('.list-item');
+const solutionTextEl = document.querySelector('.solution-text');
+
+const solutionsData = [
+  {
+    solutionText: "REFEED+ provides consistent and reliable access to healthy food with dignity. By using an in-store kiosk rather than a mobile app, individuals can check available surplus food and redeem items anonymously in familiar local settings. This transactional approach ensures that even those without a permanent residence or technology can secure a steady food supply without the friction of traditional aid models.",
+  },
+  {
+    solutionText: "Food businesses in Ireland face waste disposal costs of up to €500 monthly. REFEED+ offers a high-value, low-cost subscription that reduces these levies by redirecting surplus food away from bins and toward the local community. The system integrates directly into existing POS workflows, making the donation process a frictionless part of the staff routine while enhancing the business’s reputation through monthly impact reports.",
+  },
+  {
+    solutionText: "We provide these organizations with Data-as-a-Service (DaaS) subscriptions, offering real-time, anonymous data on food waste and the needs of underprivileged populations. Unlike static reports, our live dashboard and API integration allow NGOs and government bodies to monitor live data with neighborhood-level granularity. This empirical data is essential for academic research and strategic planning to address the root causes of food insecurity.",
+  }
+];
+
+if (solutionItems.length > 0) {
+  solutionItems[0].classList.add('active');
+
+  const first = solutionsData[0];
+
+  solutionTextEl.textContent = first.solutionText;
+}
+
+solutionItems.forEach((item, index) => {
+  item.addEventListener('click', () => {
+
+    solutionItems.forEach(i => i.classList.remove('active'));
+
+    item.classList.add('active');
+
+    const data = solutionsData[index];
+
+    solutionTextEl.textContent = data.solutionText;
+  });
+});
